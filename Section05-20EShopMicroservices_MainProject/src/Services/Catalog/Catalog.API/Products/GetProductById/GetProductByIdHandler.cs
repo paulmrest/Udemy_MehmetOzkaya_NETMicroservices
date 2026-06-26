8f90adc43898f81e7fa68cda13cdd9586256dc00
paulmrest <paulmrest@gmail.com>
2026-06-25 17:09:18 -0700

    Marten DB basic imp, Docker DB deployment, GetProducts and GetProductById endpoints

---
+++ Section05-20EShopMicroservices_MainProject/src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,21 @@
+namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid id) : IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(Product Product);
+
+internal class GetProductByIdQueryHandler
+  (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+  : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+  public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+  {
+    logger.LogInformation("GetProductsByIdQueryHandler.Handle called with {@Query}", query);
+
+    var product = await session.LoadAsync<Product>(query.id, cancellationToken);
+    if (product is null)
+    {
+      throw new ProductNotFoundException();
+    }
+    return new GetProductByIdResult(product);
+  }
+}
--- Section05-20EShopMicroservices_MainProject/src/Services/Catalog/Catalog.API/Program.cs
+++ Section05-20EShopMicroservices_MainProject/src/Services/Catalog/Catalog.API/Program.cs
@@ -1,13 +1,18 @@
-using Carter;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//Carter maps endpoints
 builder.Services.AddCarter();
+//MediatR mediator design pattern for APIs - abstracts and encapsulates communication between classes through a mediator object
 builder.Services.AddMediatR(config =>
 {
   config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
 });
+//Marten abstracts DB interaction
+builder.Services.AddMarten(opts =>
+{
+  opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
