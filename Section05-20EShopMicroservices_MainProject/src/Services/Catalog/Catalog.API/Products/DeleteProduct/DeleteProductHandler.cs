3ad44794c083e3893d4e49ab0c57f83f306ed557
paulmrest <paulmrest@gmail.com>
2026-07-01 11:17:45 -0700

    Exception handling for CreateProduct, DeleteProduct, and UpdateProduct

--- Section05-20EShopMicroservices_MainProject/src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ Section05-20EShopMicroservices_MainProject/src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -3,6 +3,14 @@ namespace Catalog.API.Products.DeleteProduct;
 public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
 public record DeleteProductResult(bool IsSuccess);
 
+public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+{
+  public DeleteProductCommandValidator()
+  {
+    RuleFor(command => command.Id).NotEmpty().WithMessage("Product ID is required");
+  }
+}
+
 internal class DeleteProductCommandHandler
   (IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
   : ICommandHandler<DeleteProductCommand, DeleteProductResult>
