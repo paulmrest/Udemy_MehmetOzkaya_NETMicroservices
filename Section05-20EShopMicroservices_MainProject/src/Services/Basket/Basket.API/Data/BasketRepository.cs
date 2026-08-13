050f5f7f42bdc2d05a201a8bdb62ecfb74cf8400
paulmrest <paulmrest@gmail.com>
2026-08-13 16:41:14 -0700

    Redis Cache

--- Section05-20EShopMicroservices_MainProject/src/Services/Basket/Basket.API/Data/BasketRepository.cs
+++ Section05-20EShopMicroservices_MainProject/src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -3,13 +3,6 @@ namespace Basket.API.Data;
 public class BasketRepository(IDocumentSession session)
   : IBasketRepository
 {
-  public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
-  {
-    session.Delete<ShoppingCart>(userName);
-    await session.SaveChangesAsync(cancellationToken);
-    return true;
-  }
-
   public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
   {
     var basket = await session.LoadAsync<ShoppingCart>(userName, cancellationToken);
@@ -21,5 +14,12 @@ public class BasketRepository(IDocumentSession session)
     session.Store(basket);
     await session.SaveChangesAsync(cancellationToken);
     return basket;
+  }
+  
+  public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+  {
+    session.Delete<ShoppingCart>(userName);
+    await session.SaveChangesAsync(cancellationToken);
+    return true;
   }
 }
