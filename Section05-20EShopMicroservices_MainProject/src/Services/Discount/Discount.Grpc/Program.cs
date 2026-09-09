7d5a8c102ae9f76c1c767f2d12356c0efcf0c978
paulmrest <paulmrest@gmail.com>
2026-09-08 18:11:13 -0700

    Docker containerization

--- Section05-20EShopMicroservices_MainProject/src/Services/Discount/Discount.Grpc/Program.cs
+++ Section05-20EShopMicroservices_MainProject/src/Services/Discount/Discount.Grpc/Program.cs
@@ -9,9 +9,8 @@ builder.Services.AddGrpc();
 builder.Services.AddGrpcReflection();
 
 builder.Services.AddDbContext<DiscountContext>(opts =>
-{
-  opts.UseSqlite(builder.Configuration.GetConnectionString("Database"));
-});
+  opts.UseSqlite(builder.Configuration.GetConnectionString("Database"))
+);
 
 var app = builder.Build();
 
